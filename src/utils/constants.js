976ee9a3d1b8d1d7c57a89ab653a6db67c694d11
rashmiiiii <rs8447835@gmail.com>
2025-01-19 03:12:39 +0530

    Final

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
-// const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-const GOOGLE_API_KEY = "AIzaSyDI95iIY7blMmBNhEqJr0fSIEZNoG9b37o";
+// require('dotenv').config();
+const GOOGLE_API_KEY = process.env.REACT_APP_RASHMI_API_KEY;
+console.log("THIS IS API KEY",GOOGLE_API_KEY);
 
 export const YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
